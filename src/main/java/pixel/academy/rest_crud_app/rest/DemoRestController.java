e600c3326037c339f1720ccd835b84789b95a175
Francesca Andrian <francescaandrianaaa@gmail.com>
2025-05-03 15:35:16 +0200

    Modify the class StudentRestController

--- src/main/java/pixel/academy/rest_crud_app/rest/DemoRestController.java
+++ src/main/java/pixel/academy/rest_crud_app/rest/DemoRestController.java
@@ -1,8 +1,10 @@
 package pixel.academy.rest_crud_app.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pixel.academy.rest_crud_app.entity.Student;
 
 @RestController
 @RequestMapping("/test")
@@ -14,4 +16,6 @@ public class DemoRestController {
     public String sayHello() {
         return "Hello World!";
     }
+
+
 }
--- src/main/java/pixel/academy/rest_crud_app/rest/StudentRestController.java
+++ src/main/java/pixel/academy/rest_crud_app/rest/StudentRestController.java
@@ -2,6 +2,7 @@ package pixel.academy.rest_crud_app.rest;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pixel.academy.rest_crud_app.entity.Student;
@@ -31,6 +32,13 @@ public class StudentRestController {
     public List<Student> getStudents() {
 
         return theStudents;
+    }
+
+    // definim un endpoint ca sa afisam toti studentiinostri dupa index
+    @GetMapping("/student/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        return theStudents.get(studentId);
     }
 
 }
